adcadb80b25dc7f7caf046b8dddabaa791c8a6fc
Manuel Guetio <manuel.guetio.l@uniautonoma.edu.co>
2025-03-30 11:08:52 -0500

    Se agrega micro servicio users

---
+++ msvc-users/src/test/java/org/projects/msvc_users/MsvcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projects.msvc_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
