db3f053c7648ae669e59c028e68374d3411c7e00
Manuel Guetio <manuel.guetio.l@uniautonoma.edu.co>
2025-03-30 12:36:51 -0500

    Se agregan atributos a la entidad user y se crea agreementEntity

---
+++ msvc-users/src/main/java/org/projects/msvc_users/entity/AgreementEntity.java
@@ -0,0 +1,14 @@
+package org.projects.msvc_users.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AgreementEntity {
+    
+}
--- msvc-users/src/main/java/org/projects/msvc_users/entity/UserEntity.java
+++ msvc-users/src/main/java/org/projects/msvc_users/entity/UserEntity.java
@@ -1,8 +1,73 @@
 package org.projects.msvc_users.entity;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "users")
 public class UserEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Email
+    private String email;
+
+    private String password;
+
+    private String phone;
+
+    private String address;
+
+    private String document;
+
+    @Column(name = "document_type")
+    private String documentType;
+
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    private boolean status;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "status_updated_at")
+    private Date statusUpdatedAt;
+
+    @Column(name = "data_updated_at")
+    private Date dataUpdatedAt;
+
+    @Column(name = "tutor_id")
+    private Long tutorId;
+
+    @Column(name = "agreement_id")
+    private Long agreementId;
+
+    @ManyToOne
+    @JoinColumn(name = "tutor_id")
+    private UserEntity tutor;
+
+    @ManyToOne
+    @JoinColumn(name = "agreement_id")
+    private AgreementEntity agreement;
 }
